36a5a8bc7371d66585a779e4543b9db57c86a0ee
surajitc <surajitc@INsurajitc.local>
2015-11-21 19:09:18 +0530

    added deck.js where deck have the shuffle property

--- cardGenerator.js
+++ cardGenerator.js
@@ -1,23 +1,22 @@
-var Card = function(suit,name,rank,point){
+var lib = {};
+lib.Card = function(suit,name,rank,point){
 	this.suit = suit;
 	this.name = name;
 	this.rank = rank;
 	this.point = point;
 };
 
-var generateDeckOf32Cards = function(){
+lib.generateDeckOf32Cards = function(){
 	var prioritisedCards = ['Jack',9,'Ace',10,'King','Queen',8,7];
 	var suits = ['Heart','Spade','Club','Diamond'];
 	var prioritisedPoints = [3,2,1,1,0,0,0,0];
 	var deckOfCards = [];
 	suits.forEach(function(eachSuit){
 		prioritisedCards.forEach(function(eachCard,indexOfprioritisedCards){
-			deckOfCards.push(new Card(eachSuit,eachCard,indexOfprioritisedCards + 1,prioritisedPoints[indexOfprioritisedCards]));
+			deckOfCards.push(new lib.Card(eachSuit,eachCard,indexOfprioritisedCards + 1,prioritisedPoints[indexOfprioritisedCards]));
 		});
 	});
 	return deckOfCards;
 };
 
-exports.generateDeckOf32Cards = generateDeckOf32Cards;
-exports.Card=Card;
-
+exports.lib = lib;
--- cardGeneratorTest.js
+++ cardGeneratorTest.js
@@ -1,4 +1,6 @@
-var lib = require('./cardGenerator.js');
+var lib = require('./cardGenerator.js').lib;
+var m = require('./deck.js').module;
+console.log(lib);
 var expect = require('chai').expect;
 
 var cards =[ { suit: 'Heart', name: 'Jack', rank: 1, point: 3 },
@@ -37,9 +39,12 @@ var cards =[ { suit: 'Heart', name: 'Jack', rank: 1, point: 3 },
 
 
 describe('add all attributes',function(){
-	it('should set all properties',function(){
-		expect({'name':'Jack','suit':'Heart','rank':1,'point':3}).to.deep.equal(new lib.Card('Heart','Jack',1,3));
+	it('should set all properties of Jack of Heart',function(){
+		expect({'suit':'Heart','name':'Jack','rank':1,'point':3}).to.deep.equal(new lib.Card('Heart','Jack',1,3));
 	});
+  it('should set all properties of 7 of Diamond',function(){
+    expect({'suit':'Diamond','name':'7','rank':8,'point':0}).to.deep.equal(new lib.Card('Diamond','7',8,0));
+  });
 });
 
 describe('generate all cards',function(){
@@ -49,5 +54,12 @@ describe('generate all cards',function(){
 	it('should have all card',function(){
 		expect(cards).to.eql(lib.generateDeckOf32Cards());
 	});
+});
+
+describe('shuffle the playable cards',function(){
+  it('should have 32 cards',function(){
+    var a = new m.Deck(lib.generateDeckOf32Cards());
+    expect(32).to.equal(a.shuffle.length);
+  });
 });
 
