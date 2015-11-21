833433ac2b97ccda387b5bb068d20b8799494eeb
ranjukr <ranjukr@INranjukr.local>
2015-11-21 10:30:15 +0530

    32 cards are generated and tests are also written and package.json is also being added.

---
+++ cardGenerator.js
@@ -0,0 +1,28 @@
+var lodash = require('lodash');
+var lib = {};
+exports.lib = lib;
+
+
+lib.SetProperty = function(name,suit,rank,point){
+	this.name = name;
+	this.suit = suit;
+	this.rank = rank;
+	this.point = point;
+};
+
+lib.generateCard = function(){
+	var cards = ['Jack',9,'Ace',10,'King','Queen',8,7];
+	var suit = ['Heart','Spade','Club','Diamond'];
+	var points = [3,2,1,1,0,0,0,0];
+	var allCards = [];
+	suit.forEach(function(eachSuit){
+		cards.forEach(function(eachCard,index){
+			allCards.push(new lib.SetProperty(eachCard,eachSuit,index + 1,points[index]))
+		});
+	});
+	return allCards;
+};
+
+
+
+
---
+++ cardGeneratorTest.js
@@ -0,0 +1,53 @@
+var lib = require('./cardGenerator.js').lib;
+var assert = require('assert');
+
+var cards = [ { name: 'Jack', suit: 'Heart', rank: 1, point: 3 },
+  { name: 9, suit: 'Heart', rank: 2, point: 2 },
+  { name: 'Ace', suit: 'Heart', rank: 3, point: 1 },
+  { name: 10, suit: 'Heart', rank: 4, point: 1 },
+  { name: 'King', suit: 'Heart', rank: 5, point: 0 },
+  { name: 'Queen', suit: 'Heart', rank: 6, point: 0 },
+  { name: 8, suit: 'Heart', rank: 7, point: 0 },
+  { name: 7, suit: 'Heart', rank: 8, point: 0 },
+  { name: 'Jack', suit: 'Spade', rank: 1, point: 3 },
+  { name: 9, suit: 'Spade', rank: 2, point: 2 },
+  { name: 'Ace', suit: 'Spade', rank: 3, point: 1 },
+  { name: 10, suit: 'Spade', rank: 4, point: 1 },
+  { name: 'King', suit: 'Spade', rank: 5, point: 0 },
+  { name: 'Queen', suit: 'Spade', rank: 6, point: 0 },
+  { name: 8, suit: 'Spade', rank: 7, point: 0 },
+  { name: 7, suit: 'Spade', rank: 8, point: 0 },
+  { name: 'Jack', suit: 'Club', rank: 1, point: 3 },
+  { name: 9, suit: 'Club', rank: 2, point: 2 },
+  { name: 'Ace', suit: 'Club', rank: 3, point: 1 },
+  { name: 10, suit: 'Club', rank: 4, point: 1 },
+  { name: 'King', suit: 'Club', rank: 5, point: 0 },
+  { name: 'Queen', suit: 'Club', rank: 6, point: 0 },
+  { name: 8, suit: 'Club', rank: 7, point: 0 },
+  { name: 7, suit: 'Club', rank: 8, point: 0 },
+  { name: 'Jack', suit: 'Diamond', rank: 1, point: 3 },
+  { name: 9, suit: 'Diamond', rank: 2, point: 2 },
+  { name: 'Ace', suit: 'Diamond', rank: 3, point: 1 },
+  { name: 10, suit: 'Diamond', rank: 4, point: 1 },
+  { name: 'King', suit: 'Diamond', rank: 5, point: 0 },
+  { name: 'Queen', suit: 'Diamond', rank: 6, point: 0 },
+  { name: 8, suit: 'Diamond', rank: 7, point: 0 },
+  { name: 7, suit: 'Diamond', rank: 8, point: 0 } ];
+
+
+
+describe('add all attributes',function(){
+	it('should set all properties',function(){
+		assert.deepEqual({'name':'Jack','suit':'Heart','rank':1,'point':3},new lib.SetProperty('Jack','Heart',1,3))
+	});
+});
+
+describe('generate all cards',function(){
+	it('should have 32 cards',function(){
+		assert.equal(32,lib.generateCard().length);
+	});
+	it('should have all card',function(){
+		assert.deepEqual(cards,lib.generateCard());
+	});
+});
+
