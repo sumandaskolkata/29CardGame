3fbef8a32d1c9617ba9bc9a8d0d54804d6caad49
shrutic <shrutic@INshrutic.local>
2015-11-23 11:48:39 +0530

    modified playerTest.js

--- playerTest.js
+++ playerTest.js
@@ -6,7 +6,7 @@ describe('player',function(){
 		var player = new lib.Player('anjaly');
 		expect(player).to.have.all.keys('cards','bidPoints');
 	});
-	it('should have bidPoints with properties',function(){
+	it('should have bidPoints field with properties',function(){
 		var player = new lib.Player('anjaly');
 		expect(player.bidPoints(16)).to.have.all.keys('ID','bidPoint');
 		expect(player.bidPoints(16)).to.be.a('object');
