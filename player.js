38ec3854cbaa95360cc6a18f577f7cf742f3408c
surajitc <surajitc@INsurajitc.local>
2015-11-23 10:17:50 +0530

    player.js has been modified with good variable name

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
 var ld = require('lodash');
 
-var makeTwoTeams = function(playersID){
-	var randomisePlayers = ld.shuffle(playersID);
-	var seperatedTeams = ld.chunk(randomisePlayers,2);
+var makeTwoTeams = function(playersUniqueIDs){
+	var randomisePlayersUniqueIDs = ld.shuffle(playersUniqueIDs);
+	var seperatedTeams = ld.chunk(randomisePlayersUniqueIDs,2);
 	return seperatedTeams;
 };
 var Player = function (playerUniqueID) {
 	this.cards = [];
-	this.bidPoints = function (point) {
-		return {ID:playerUniqueID,point:point};
+	this.bidPoints = function (assumingBidPoint) {
+		return {ID:playerUniqueID,bidPoint:assumingBidPoint};
 	};
 };
