251fe24b967ba780dfe9b70d7495cb21a0ca99ad
sumand <sumand@INsumand.local>
2015-11-23 13:30:26 +0530

    player.js modified with monitor module

--- player.js
+++ player.js
@@ -1,13 +1,36 @@
 var ld = require('lodash');
+var module = {};
+exports.module = module;
+
 
 var makeTwoTeams = function(playersID){
 	var randomisePlayers = ld.shuffle(playersID);
 	var seperatedTeams = ld.chunk(randomisePlayers,2);
 	return seperatedTeams;
 };
-var Player = function (playerUniqueID) {
+module.Player = function (playerUniqueID) {
+	this.playerId = playerUniqueID;
 	this.cards = [];
 	this.bidPoints = function (point) {
 		return {ID:playerUniqueID,point:point};
 	};
 };
+module.Player.prototype = {
+
+};
+
+module.Team = function(team){
+	this.player1 = new module.Player(team[0]);
+	this.player2 = new module.Player(team[1]);
+
+
+}
+module.Team.prototype = {
+
+}
+var twoTeam = makeTwoTeams(['shruti','surajit','jishnu','ranju'])
+module.monitor = {};
+module.monitor.team1 = new module.Team(twoTeam[0]);
+module.monitor.team2 = new module.Team(twoTeam[1]);
+
+
