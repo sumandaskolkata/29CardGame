f85cd7afcb06fd5213f25c76f7d1d285c3cba864
anjalyj <anjalyj@INanjalyj.local>
2015-11-23 10:34:53 +0530

    player.js modified

--- player.js
+++ player.js
@@ -1,11 +1,11 @@
 var ld = require('lodash');
 
-var makeTwoTeams = function(playersID){
-	var randomisePlayers = ld.shuffle(playersID);
-	var seperatedTeams = ld.chunk(randomisePlayers,2);
+lib.makeTwoTeams = function(playersUniqueIDs){
+	var randomisePlayersUniqueIDs = ld.shuffle(playersUniqueIDs);
+	var seperatedTeams = ld.chunk(randomisePlayersUniqueIDs,2);
 	return seperatedTeams;
 };
-var Player = function (playerUniqueID) {
+lib.Player = function (playerUniqueID) {
 	this.cards = [];
 	this.bidPoints = function (point) {
 		return {ID:playerUniqueID,point:point};
