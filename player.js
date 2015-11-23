3fd6cdcd714afa56d9573d19de6fabc873edf5fa
anjalyj <anjalyj@INanjalyj.local>
2015-11-23 16:15:57 +0530

    player.js is modified with pair properties

--- player.js
+++ player.js
@@ -11,17 +11,26 @@ lib.makeTwoTeams = function(playersUniqueIDs){
 lib.Player = function (playerUniqueID) {
 	this.playerID=playerUniqueID
 	this.hand = [];
-	this.bidPoints = function (assumingBidPoint) {
-		return {ID:playerUniqueID,bidPoint:assumingBidPoint};
-	};
+	this.isBidder = false;
+	this.hasPair = false;
+	
 };
-lib.Team = function(team){
+lib.Player.prototype= {
+	bidPoints : function (assumingBidPoint) {
+		return {ID:this.playerID,bidPoint:assumingBidPoint};
+	},
+};
+lib.Team = function(team){		
 	this.player1 = new lib.Player(team[0]);
 	this.player2 = new lib.Player(team[1]);
-
-
-}
+	this.isPair = false;
+	this.wonCardsBucket = [];
+	this.score = 0;
+};
 lib.Team.prototype = {
+	scoreBoard : function(isWinner){
+		(isWinner == true) ? this.score ++ : this.score --;
+	}	
 
 }
 var twoTeam = lib.makeTwoTeams(['shruti','surajit','jishnu','ranju'])
--- test/playerTest.js
+++ test/playerTest.js
@@ -12,14 +12,49 @@ describe('makeTwoTeams',function(){
 	});
 });
 
-describe('player',function(){
+describe('Player',function(){
+	var player = new lib.Player('anjaly');
 	it('should have properties',function(){
-		var player = new lib.Player('anjaly');
-		expect(player).to.have.all.keys('playerID','hand','bidPoints');
+		expect(player).to.have.all.keys('playerID','hand','isBidder','hasPair');
 	});
 	it('should have bidPoints with properties',function(){
-		var player = new lib.Player('anjaly');
 		expect(player.bidPoints(16)).to.have.all.keys('ID','bidPoint');
 		expect(player.bidPoints(16)).to.be.a('object');
+		expect(player.bidPoints(18).bidPoint).to.equal(18);
+		expect(player.bidPoints(20).ID).to.equal('anjaly');
 	});
+	it('isBidder should be initialized with false',function(){
+		expect(player.isBidder).to.be.false;
+	});
+	it('hasPair should be initialized with false',function(){
+		expect(player.hasPair).to.be.false;
+	});
+});
+
+describe('Team',function(){
+	var team = new lib.Team(['anjaly','jishnu']);
+	it('should have created player1 with 1st team member & player2 with 2nd team member',function(){
+		expect(team.player1.playerID).to.equal('anjaly');
+		expect(team.player2.playerID).to.equal('jishnu');
+	})
+	it('should have properties',function(){
+		expect(team).to.have.all.keys('player1','player2','isPair','wonCardsBucket','score');
+	});
+	it('should have initially hasPair property false',function(){
+		expect(team.isPair).to.be.false;
+	});
+	it('should have score initially with value 0',function(){
+		expect(team.score).to.equal(0);
+	});
+	it('should increase the score when team wins',function(){
+		team.score=2;
+		var finalScore=team.scoreBoard(true);
+		expect(team.score).to.equal(3);
+	});
+	it('should decrease the score when team loses',function(){
+		team.score=2;
+		var finalScore=team.scoreBoard(false);
+		expect(team.score).to.equal(1);
+	});
+
 });
